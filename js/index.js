0ef7ad4442b02b26b7d75ea74b36dba891f7cf4a
Johan Strootman <johanstr@users.noreply.github.com>
2022-02-09 16:52:09 +0100

    Resultaat IM van 09-02-2022
    Resultaat van het instructiemoment van 9 februari 2022

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Winkelwagen daat structuur
+// Winkelwagen data structuur
 let winkelwagen = [
     {
         id: 1,
@@ -25,10 +25,52 @@ async function loadGames()
         .then(response => response.json())
         .then(data => {
             games = data;
-            console.log(games);
+            localStorage.setItem('games', JSON.stringify(games));
+
+            showGames();
         })
         .catch(error => console.log(error));
 }
 
 
+function showGames()
+{
+    games.forEach((game) => {
+        let gamecard = `
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img
+                        class="bd-placeholder-img card-img-top game-img-top" 
+                        src="${game.image}"
+                        alt="${game.title}"
+                        title="${game.title}"
+                    />
+                    <div class="card-body">
+                        <p class="card-text">
+                            ${game.description}
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-primary">
+                                    <i class="fa fa-info"></i>&nbsp;Info
+                                </button>
+                                <button
+                                    type="button"
+                                    class="btn btn-success"
+                                >
+                                    <i class="fas fa-shopping-cart"></i>&nbsp;Kopen
+                                </button>
+                            </div>
+                            <small class="text-muted price-tag">
+                                &euro; ${game.price}
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `; 
+
+        gamecard_container.innerHTML += gamecard;
+    });
+}
 
